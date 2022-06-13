d1d750515d4dee4e2a1ed6e5f9e93d981ebc4f87
Sido Balazs <astrall.bzs@gmail.com>
2022-06-13 13:44:21 +0200

    a6

--- src/Cheer.java
+++ src/Cheer.java
@@ -1,6 +1,6 @@
 public class Cheer {
 
-    public void cheer(){
-        System.out.println("java");
+    public void cheer(String parameter){
+        System.out.println("Cheer for the " + parameter);
     }
 }
