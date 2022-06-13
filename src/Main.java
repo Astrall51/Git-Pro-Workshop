278a248bc25246b4a8a8d994aa355c3aeeddc844
bbenk260 <99719402+bbenk260@users.noreply.github.com>
2022-06-13 11:05:41 +0200

    b1

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         greet();
+        count();
     }
 
     public static void greet(){
         System.out.println("Sidó Balázs");
+    }
+    public static void count(){
+        System.out.println("28");
     }
 }
