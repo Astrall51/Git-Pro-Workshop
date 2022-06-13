1f1eb297c7798772cf8a5e84e0c873d1d7d45392
Sido Balazs <astrall.bzs@gmail.com>
2022-06-13 11:20:24 +0200

    Correction

---
+++ src/Greeter.java
@@ -0,0 +1,5 @@
+public class Greeter {
+    public static void greet(String name){
+        System.out.println(name);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,4 @@
 public class Main {
     public static void main(String[] args) {
-        greet("Balazs");
-        count();
-    }
-
-    public static void greet(String name){
-        System.out.println(name);
-    }
-
-    public static void cheer() {
-        System.out.println("BBG");
-    }
-
-    public static void count(){
-        System.out.println("28");
-
     }
 }
