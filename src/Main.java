fcf0bd715e0a7d4979c916366b46138cb5e88788
Vasvári Gergő <gergo.vasvari@gmail.com>
2022-06-13 11:14:27 +0200

    c2

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,18 @@ public class Main {
     public static void main(String[] args) {
         greet();
         count();
+        cheer();
     }
 
     public static void greet(){
         System.out.println("Sidó Balázs");
     }
-<<<<<<< HEAD
 
-    public static void cheer(){
-        System.out.println("BBG");
-=======
+    public static void cheer(String teamNAme) {
+        System.out.println(teamNAme);
+    }
+
     public static void count(){
         System.out.println("28");
->>>>>>> 278a248bc25246b4a8a8d994aa355c3aeeddc844
     }
 }
