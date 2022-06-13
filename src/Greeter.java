08577b0856b2a746db21c9ff8778177d52a679ab
bbenk260 <99719402+bbenk260@users.noreply.github.com>
2022-06-13 14:08:37 +0200

    Completes Geri's task

--- src/Greeter.java
+++ src/Greeter.java
@@ -1,10 +1,5 @@
 public class Greeter {
-<<<<<<< HEAD
-
     public static void greet(String name) {
-=======
-    public static void greet(String name){
->>>>>>> b2a60e0a3c7c7d12e1a14f60edcab87847cf95da
         System.out.println("Greetings for the " + name);
     }
 }
