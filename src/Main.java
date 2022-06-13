de509635b1384d42c0d6abedd6612557373baeaf
bbenk260 <99719402+bbenk260@users.noreply.github.com>
2022-06-13 11:20:51 +0200

    b2

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         greet();
-        count(28);
     }
 
     public static void greet() {
         System.out.println("Sidó Balázs");
         greet("Balazs");
-        count(28);
     }
 
     public static void greet(String name) {
@@ -16,10 +14,6 @@ public class Main {
 
     public static void cheer() {
         System.out.println("BBG");
-    }
-
-    public static void count(int age) {
-        System.out.println(age);
     }
 }
 
